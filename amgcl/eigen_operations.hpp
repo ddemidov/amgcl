566d7093631217081a0666139a3cc7cde3a6ccc4
Denis Demidov <ddemidov@ksu.ru>
2012-12-04 15:39:13 +0400

    Corrected result type for eigen inner_prod

--- amgcl/eigen_operations.hpp
+++ amgcl/eigen_operations.hpp
@@ -9,7 +9,7 @@ struct value_type {
 };
 
 template <typename T1, typename T2>
-double inner_prod(const Eigen::MatrixBase<T1> &x, const Eigen::MatrixBase<T2> &y) {
+typename T1::Scalar inner_prod(const Eigen::MatrixBase<T1> &x, const Eigen::MatrixBase<T2> &y) {
     return x.dot(y);
 }
 
