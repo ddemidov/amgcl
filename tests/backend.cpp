7b816866876e06551296ee50de4772430d5b9ce7
Denis Demidov <dennis.demidov@gmail.com>
2014-06-01 22:39:37 +0400

    Test SPMV for each CPU backend

---
+++ tests/backend.cpp
@@ -0,0 +1,105 @@
+#define BOOST_TEST_MODULE MatrixCRS
+#include <set>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/random.hpp>
+#include <boost/range.hpp>
+#include <boost/range/combine.hpp>
+#include <boost/foreach.hpp>
+#include <boost/mpl/list.hpp>
+
+#include <amgcl/backend/builtin.hpp>
+#include <amgcl/backend/block_crs.hpp>
+#include <amgcl/backend/eigen.hpp>
+#include <amgcl/backend/ccrs.hpp>
+
+template <typename P, typename C, typename V>
+void random_problem(size_t n, size_t m, size_t nnz_per_row,
+        std::vector<P> &ptr,
+        std::vector<C> &col,
+        std::vector<V> &val,
+        std::vector<V> &vec
+        )
+{
+    ptr.reserve(n + 1);
+    col.reserve(nnz_per_row * n);
+
+    boost::random::mt19937 rng;
+    boost::random::uniform_int_distribution<C> random_column(0, m - 1);
+
+    ptr.push_back(0);
+    for(size_t i = 0; i < n; i++) {
+        std::set<C> cs;
+        for(size_t j = 0; j < nnz_per_row; ++j)
+            cs.insert(random_column(rng));
+
+        BOOST_FOREACH(C c, cs) col.push_back(c);
+
+        ptr.push_back(static_cast<P>( col.size() ));
+    }
+
+    boost::random::uniform_real_distribution<V> random_value(0, 1);
+
+    val.resize( col.size() );
+    BOOST_FOREACH(V &v, val) v = random_value(rng);
+
+    vec.resize(n);
+    BOOST_FOREACH(V &v, vec) v = random_value(rng);
+}
+
+template <class Backend>
+void test_backend() {
+    typedef typename Backend::value_type V;
+    typedef typename Backend::index_type I;
+
+    typedef typename Backend::matrix  matrix;
+    typedef typename Backend::vector  vector;
+    typedef typename Backend::params  params;
+
+    typedef amgcl::backend::crs<V, I> ref_matrix;
+
+    const size_t n = 256;
+
+    std::vector<I> ptr;
+    std::vector<I> col;
+    std::vector<V> val;
+    std::vector<V> vec;
+
+    random_problem(n, n, 16, ptr, col, val, vec);
+
+    params prm;
+
+    boost::shared_ptr<ref_matrix> Aref = boost::make_shared<ref_matrix>(n, n, ptr, col, val);
+    boost::shared_ptr<matrix>     Atst = Backend::copy_matrix(Aref, prm);
+
+    boost::shared_ptr<vector> x = Backend::copy_vector(vec, prm);
+    boost::shared_ptr<vector> y = Backend::create_vector(n, prm);
+
+    amgcl::backend::clear(*y);
+
+    std::vector<V> y_ref(n, 0);
+
+    amgcl::backend::spmv(2, *Aref, vec, 1, y_ref);
+    amgcl::backend::spmv(2, *Atst, *x,  1, *y);
+
+    for(size_t i = 0; i < n; ++i)
+        BOOST_CHECK_CLOSE((*y)[i], y_ref[i], 1e-4);
+}
+
+BOOST_AUTO_TEST_SUITE( backend_crs )
+
+typedef boost::mpl::list<
+    amgcl::backend::block_crs<float>,
+    amgcl::backend::block_crs<double>,
+    amgcl::backend::eigen<float>,
+    amgcl::backend::eigen<double>,
+    amgcl::backend::compressed_crs<float>,
+    amgcl::backend::compressed_crs<double>
+    > backends;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(construct, Backend, backends)
+{
+    test_backend<Backend>();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
