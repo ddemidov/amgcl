01ed9eb0edfa6803360ada722a27855c3fadcb0d
Denis Demidov <dennis.demidov@gmail.com>
2014-06-02 09:19:16 +0400

    Merged builder and solver classes into amgcl::amg

--- amgcl/amgcl.hpp
+++ amgcl/amgcl.hpp
@@ -31,20 +31,232 @@ THE SOFTWARE.
  * \brief  Generic algebraic multigrid framework.
  */
 
-#include <amgcl/builder.hpp>
-#include <amgcl/solver.hpp>
+#include <iostream>
+#include <iomanip>
+
+#include <boost/io/ios_state.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/ptr_container/ptr_list.hpp>
+#include <boost/foreach.hpp>
+#include <boost/tuple/tuple.hpp>
+
+#include <amgcl/backend/builtin.hpp>
+#include <amgcl/relaxation/interface.hpp>
+#include <amgcl/tictoc.hpp>
 
 namespace amgcl {
 
-template <class Builder, class Solver>
-Solver make_solver(
-        typename Builder::matrix const &A,
-        typename Builder::params const &build_prm,
-        typename Solver::params  const &solve_prm
-        )
-{
-    return Solver(Builder(A, build_prm), solve_prm);
-}
+template <
+    class Backend,
+    class Coarsening,
+    relaxation::scheme Relax
+    >
+class amg {
+    public:
+        typedef Backend backend_type;
+
+        typedef typename Backend::value_type value_type;
+        typedef typename Backend::matrix     matrix;
+        typedef typename Backend::vector     vector;
+
+        typedef relaxation::impl<Relax, Backend> relax_type;
+
+        struct params {
+            typedef typename Backend::params    backend_params;
+            typedef typename Coarsening::params coarsening_params;
+            typedef typename relax_type::params relax_params;
+
+            backend_params    backend;
+            coarsening_params coarsening;
+            relax_params      relax;
+
+            int coarse_enough;
+
+            int npre;
+            int npost;
+            int ncycle;
+
+            params() :
+                coarse_enough( 300 ),
+                npre         (   1 ),
+                npost        (   1 ),
+                ncycle       (   1 )
+            {}
+        } prm;
+
+        template <class Matrix>
+        amg(const Matrix &M, const params &prm = params())
+            : prm(prm)
+        {
+            precondition(
+                    backend::rows(M) == backend::cols(M),
+                    "Matrix should be square!"
+                    );
+
+            boost::shared_ptr<build_matrix> A = boost::make_shared<build_matrix>( M );
+            boost::shared_ptr<build_matrix> P, R;
+
+            while( backend::rows(*A) > prm.coarse_enough) {
+                TIC("transfer operators");
+                boost::tie(P, R) = Coarsening::transfer_operators(*A, prm.coarsening);
+                TOC("transfer operators");
+
+                TIC("move to backend")
+                levels.push_back( new level(A, P, R, prm) );
+                TOC("move to backend")
+
+                TIC("coarse operator");
+                A = Coarsening::coarse_operator(*A, *P, *R, prm.coarsening);
+                sort_rows(*A);
+                TOC("coarse operator");
+            }
+
+            TIC("coarsest level");
+            boost::shared_ptr<build_matrix> Ainv = boost::make_shared<build_matrix>();
+            *Ainv = inverse(*A);
+            TOC("coarsest level");
+
+            TIC("move to backend")
+            levels.push_back( new level(Ainv, prm) );
+            TOC("move to backend")
+        }
+
+        void apply(const vector &rhs, vector &x) const {
+            cycle(levels.begin(), rhs, x);
+        }
+
+        void operator()(const vector &rhs, vector &x) const {
+            apply(rhs, x);
+        }
+
+        const matrix& top_matrix() const {
+            return *levels.front().A;
+        }
+    private:
+        typedef typename backend::builtin<value_type>::matrix build_matrix;
+
+        struct level {
+            boost::shared_ptr<matrix> A;
+            boost::shared_ptr<matrix> P;
+            boost::shared_ptr<matrix> R;
+
+            boost::shared_ptr<vector> f;
+            boost::shared_ptr<vector> u;
+            boost::shared_ptr<vector> t;
+
+            boost::shared_ptr<relax_type> relax;
+
+            level(
+                    boost::shared_ptr<build_matrix> a,
+                    boost::shared_ptr<build_matrix> p,
+                    boost::shared_ptr<build_matrix> r,
+                    const params &prm
+                 ) :
+                A( Backend::copy_matrix(a, prm.backend) ),
+                P( Backend::copy_matrix(p, prm.backend) ),
+                R( Backend::copy_matrix(r, prm.backend) ),
+                f( Backend::create_vector(backend::rows(*a), prm.backend) ),
+                u( Backend::create_vector(backend::rows(*a), prm.backend) ),
+                t( Backend::create_vector(backend::rows(*a), prm.backend) ),
+                relax( new relax_type(*a, prm.relax, prm.backend) )
+            { }
+
+            level(
+                    boost::shared_ptr<build_matrix> a,
+                    const params &prm
+                 ) :
+                A( Backend::copy_matrix(a, prm.backend) ),
+                f( Backend::create_vector(backend::rows(*a), prm.backend) ),
+                u( Backend::create_vector(backend::rows(*a), prm.backend) )
+            { }
+
+            size_t rows() const {
+                return backend::rows(*A);
+            }
+
+            size_t nonzeros() const {
+                return backend::nonzeros(*A);
+            }
+        };
+
+        typedef typename boost::ptr_list<level>::const_iterator level_iterator;
+
+        boost::ptr_list<level> levels;
+
+        void cycle(level_iterator lvl, const vector &rhs, vector &x) const
+        {
+            level_iterator nxt = lvl; ++nxt;
+
+            if (nxt == levels.end()) {
+                TIC("coarse");
+                backend::spmv(1, *lvl->A, rhs, 0, x);
+                TOC("coarse");
+            } else {
+                for (int j = 0; j < prm.ncycle; ++j) {
+                    TIC("relax");
+                    for(int i = 0; i < prm.npre; ++i)
+                        lvl->relax->apply_pre(*lvl->A, rhs, x, *lvl->t, prm.relax);
+                    TOC("relax");
+
+                    TIC("residual");
+                    backend::residual(rhs, *lvl->A, x, *lvl->t);
+                    TOC("residual");
+
+                    TIC("restrict");
+                    backend::spmv(1, *lvl->R, *lvl->t, 0, *nxt->f);
+                    TOC("restrict");
+
+                    backend::clear(*nxt->u);
+                    cycle(nxt, *nxt->f, *nxt->u);
+
+                    TIC("prolongate");
+                    backend::spmv(1, *lvl->P, *nxt->u, 1, x);
+                    TOC("prolongate");
+
+                    TIC("relax");
+                    for(int i = 0; i < prm.npre; ++i)
+                        lvl->relax->apply_post(*lvl->A, rhs, x, *lvl->t, prm.relax);
+                    TOC("relax");
+                }
+            }
+        }
+
+    friend std::ostream& operator<<(std::ostream &os, const amg &a)
+    {
+        boost::io::ios_all_saver stream_state(os);
+
+        size_t sum_dof = 0;
+        size_t sum_nnz = 0;
+
+        BOOST_FOREACH(const level &lvl, a.levels) {
+            sum_dof += lvl.rows();
+            sum_nnz += lvl.nonzeros();
+        }
+
+        os << "Number of levels:    "   << a.levels.size()
+           << "\nOperator complexity: " << std::fixed << std::setprecision(2)
+                                        << 1.0 * sum_nnz / a.levels.front().nonzeros()
+           << "\nGrid complexity:     " << std::fixed << std::setprecision(2)
+                                        << 1.0 * sum_dof / a.levels.front().rows()
+           << "\n\nlevel     unknowns       nonzeros\n"
+           << "---------------------------------\n";
+
+        size_t depth = 0;
+        BOOST_FOREACH(const level &lvl, a.levels) {
+            os << std::setw(5)  << depth++
+               << std::setw(13) << lvl.rows()
+               << std::setw(15) << lvl.nonzeros() << " ("
+               << std::setw(5) << std::fixed << std::setprecision(2)
+               << 100.0 * lvl.nonzeros() / sum_nnz
+               << "%)" << std::endl;
+        }
+
+        return os;
+    }
+
+};
 
 } // namespace amgcl
 
--- examples/solver.cpp
+++ examples/solver.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+
+#include <amgcl/amgcl.hpp>
+
 #include <amgcl/backend/crs_tuple.hpp>
 #include <amgcl/backend/block_crs.hpp>
 #include <amgcl/backend/eigen.hpp>
+
 #include <amgcl/coarsening/aggregation.hpp>
+
 #include <amgcl/relaxation/gauss_seidel.hpp>
 #include <amgcl/relaxation/damped_jacobi.hpp>
-#include <amgcl/builder.hpp>
-#include <amgcl/solver.hpp>
+
 #include <amgcl/solver/cg.hpp>
+
 #include <amgcl/profiler.hpp>
+
 #include "sample_problem.hpp"
 
 namespace amgcl {
@@ -18,44 +24,38 @@ namespace amgcl {
 int main() {
     using amgcl::prof;
 
-    typedef amgcl::backend::block_crs<double> backend;
-
-    typedef amgcl::builder<
-        double, amgcl::coarsening::aggregation
-        > Builder;
-
     typedef amgcl::amg<
-        backend, amgcl::relaxation::damped_jacobi
+        amgcl::backend::block_crs<double>,
+        amgcl::coarsening::aggregation,
+        amgcl::relaxation::damped_jacobi
         > AMG;
 
     std::vector<int>    ptr;
     std::vector<int>    col;
     std::vector<double> val;
-    backend::vector     rhs;
+    std::vector<double> rhs;
 
     prof.tic("assemble");
     int n = sample_problem(128, val, col, ptr, rhs);
     prof.toc("assemble");
 
     prof.tic("build");
-    Builder builder( boost::tie(n, n, val, col, ptr) );
+    AMG amg( boost::tie(n, n, val, col, ptr) );
     prof.toc("build");
 
-    std::cout << builder << std::endl;
+    std::cout << amg << std::endl;
 
-    prof.tic("move");
-    AMG amg(builder);
-    prof.toc("move");
+    boost::shared_ptr<AMG::vector> f = AMG::backend_type::copy_vector(rhs, amg.prm.backend);
+    boost::shared_ptr<AMG::vector> x = AMG::backend_type::create_vector(n, amg.prm.backend);
 
-    backend::vector x(n);
-    amgcl::backend::clear(x);
+    amgcl::backend::clear(*x);
 
-    amgcl::solver::cg<backend> solve(n);
-    size_t iters;
-    double resid;
+    amgcl::solver::cg<AMG::backend_type> solve(n);
 
     prof.tic("solve");
-    boost::tie(iters, resid) = solve(amg.top_matrix(), rhs, amg, x);
+    size_t iters;
+    double resid;
+    boost::tie(iters, resid) = solve(amg.top_matrix(), *f, amg, *x);
     prof.toc("solve");
 
     std::cout << "Iterations: " << iters << std::endl
