aaf27627ef4389def368944a41a9892705330881
Denis Demidov <dennis.demidov@gmail.com>
2014-07-10 23:08:16 +0400

    Adding Riccardo Rossi to deflation copyright notice

--- amgcl/adapter/epetra.hpp
+++ amgcl/adapter/epetra.hpp
@@ -5,6 +5,7 @@
 The MIT License
 
 Copyright (c) 2012-2014 Denis Demidov <dennis.demidov@gmail.com>
+Copyright (c) 2014, Riccardo Rossi, CIMNE (International Center for Numerical Methods in Engineering)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
