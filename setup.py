6d5bb88d0b9238d87cdbf3efefd7b8dc2133ad2b
Denis Demidov <dennis.demidov@gmail.com>
2014-09-27 10:39:12 +0400

    Use git version in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,80 @@
 from setuptools import setup, Extension
+from subprocess import Popen, PIPE
 import os, sys
 import numpy
 
 pyver = sys.version_info
 
+#----------------------------------------------------------------------------
+# Get version string from git
+# Author: Douglas Creager <dcreager@dcreager.net>
+# http://dcreager.net/2010/02/10/setuptools-git-version-numbers/
+#----------------------------------------------------------------------------
+def call_git_describe(abbrev=4):
+    try:
+        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
+                  stdout=PIPE, stderr=PIPE)
+        p.stderr.close()
+        line = p.stdout.readlines()[0]
+        return line.strip()
+
+    except:
+        return None
+
+
+def read_release_version():
+    try:
+        f = open("RELEASE-VERSION", "r")
+
+        try:
+            version = f.readlines()[0]
+            return version.strip()
+
+        finally:
+            f.close()
+
+    except:
+        return None
+
+
+def write_release_version(version):
+    f = open("RELEASE-VERSION", "w")
+    f.write("%s\n" % version)
+    f.close()
+
+
+def get_git_version(abbrev=4):
+    # Read in the version that's currently in RELEASE-VERSION.
+
+    release_version = read_release_version()
+
+    # First try to get the current version using "git describe".
+
+    version = call_git_describe(abbrev)
+
+    # If that doesn't work, fall back on the value that's in
+    # RELEASE-VERSION.
+
+    if version is None:
+        version = release_version
+
+    # If we still don't have anything, that's an error.
+
+    if version is None:
+        raise ValueError("Cannot find the version number!")
+
+    # If the current version is different from what's in the
+    # RELEASE-VERSION file, update the file to be current.
+
+    if version != release_version:
+        write_release_version(version)
+
+    # Finally, return the current version.
+
+    return version
+
+
+#----------------------------------------------------------------------------
 def find_file(filename, search_dirs):
     for dirname in search_dirs:
         for root, dirs, files in os.walk(dirname):
@@ -40,7 +111,7 @@ def boost_python_lib():
 
 setup(
         name='pyamgcl',
-        version='0.5.0',
+        version=get_git_version(),
         description='Solution of large sparse linear systems with Algebraic Multigrid Method',
         author='Denis Demidov',
         author_email='dennis.demidov@gmail.com',
