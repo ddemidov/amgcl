75cfdc9097390a1c0b1d65cef28332d7dd878505
Denis Demidov <dennis.demidov@gmail.com>
2015-03-20 22:26:12 +0300

    Fixed years in subdomain_deflation copyright notice

--- amgcl/mpi/subdomain_deflation.hpp
+++ amgcl/mpi/subdomain_deflation.hpp
@@ -5,7 +5,7 @@
 The MIT License
 
 Copyright (c) 2012-2015 Denis Demidov <dennis.demidov@gmail.com>
-Copyright (c) 2014, Riccardo Rossi, CIMNE (International Center for Numerical Methods in Engineering)
+Copyright (c) 2014-2015, Riccardo Rossi, CIMNE (International Center for Numerical Methods in Engineering)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
