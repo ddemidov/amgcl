99db8ac8134db6641a70c365ecf489848436a64c
Denis Demidov <dennis.demidov@gmail.com>
2015-11-08 20:29:14 +0300

    Allow generation of complex-valued sample problems

--- tests/sample_problem.hpp
+++ tests/sample_problem.hpp
@@ -1,6 +1,23 @@
 #ifndef TESTS_SAMPLE_PROBLEM_HPP
 #define TESTS_SAMPLE_PROBLEM_HPP
 
+#include <complex>
+#include <boost/type_traits.hpp>
+
+template <typename real>
+struct make_one {
+    static real get() {
+        return static_cast<real>(1);
+    }
+};
+
+template <typename real>
+struct make_one< std::complex<real> > {
+    static std::complex<real> get() {
+        return std::complex<real>(make_one<real>::get(), make_one<real>::get());
+    }
+};
+
 // Generates matrix for poisson problem in a unit cube.
 template <typename real, typename index>
 int sample_problem(
@@ -23,23 +40,25 @@ int sample_problem(
     val.reserve(n3 * 7);
     rhs.reserve(n3);
 
+    real one = make_one<real>::get();
+
     ptr.push_back(0);
     for(ptrdiff_t k = 0, idx = 0; k < n; ++k) {
         for(ptrdiff_t j = 0; j < n; ++j) {
             for (ptrdiff_t i = 0; i < n; ++i, ++idx) {
                 if (k > 0) {
                     col.push_back(idx - n * n);
-                    val.push_back(-1.0/6.0);
+                    val.push_back(-1.0/6.0 * one);
                 }
 
                 if (j > 0) {
                     col.push_back(idx - n);
-                    val.push_back(-1.0/6.0);
+                    val.push_back(-1.0/6.0 * one);
                 }
 
                 if (i > 0) {
                     col.push_back(idx - 1);
-                    val.push_back(-1.0/6.0);
+                    val.push_back(-1.0/6.0 * one);
                 }
 
                 col.push_back(idx);
@@ -47,20 +66,20 @@ int sample_problem(
 
                 if (i + 1 < n) {
                     col.push_back(idx + 1);
-                    val.push_back(-1.0/6.0);
+                    val.push_back(-1.0/6.0 * one);
                 }
 
                 if (j + 1 < n) {
                     col.push_back(idx + n);
-                    val.push_back(-1.0/6.0);
+                    val.push_back(-1.0/6.0 * one);
                 }
 
                 if (k + 1 < n) {
                     col.push_back(idx + n * n);
-                    val.push_back(-1.0/6.0);
+                    val.push_back(-1.0/6.0 * one);
                 }
 
-                rhs.push_back(1);
+                rhs.push_back( one );
                 ptr.push_back( static_cast<index>(col.size()) );
             }
         }
