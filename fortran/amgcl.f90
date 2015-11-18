74a5e26b07f33fd183e6e990d8658df2117b50b6
Denis Demidov <dennis.demidov@gmail.com>
2015-11-18 21:20:28 +0300

    Specify intent for fortran subroutines

--- fortran/amgcl.f90
+++ fortran/amgcl.f90
@@ -14,46 +14,46 @@ module amgcl
 
         subroutine amgcl_params_seti(prm, name, val) bind (C, name="amgcl_params_seti")
             use iso_c_binding
-            integer   (c_size_t), value :: prm
-            character (c_char)          :: name(*)
-            integer   (c_int),    value :: val
+            integer   (c_size_t), intent(in), value :: prm
+            character (c_char),   intent(in)        :: name(*)
+            integer   (c_int),    intent(in), value :: val
         end subroutine
 
         subroutine amgcl_params_setf(prm, name, val) bind (C, name="amgcl_params_setf")
             use iso_c_binding
-            integer   (c_size_t), value :: prm
-            character (c_char)          :: name(*)
-            real      (c_float),  value :: val
+            integer   (c_size_t), intent(in), value :: prm
+            character (c_char),   intent(in)        :: name(*)
+            real      (c_float),  intent(in), value :: val
         end subroutine
 
         subroutine amgcl_params_sets(prm, name, val) bind (C, name="amgcl_params_sets")
             use iso_c_binding
-            integer   (c_size_t), value :: prm
-            character (c_char)          :: name(*)
-            character (c_char)          :: val(*)
+            integer   (c_size_t), intent(in), value :: prm
+            character (c_char),   intent(in)        :: name(*)
+            character (c_char),   intent(in)        :: val(*)
         end subroutine
 
         subroutine amgcl_params_destroy(prm) bind(C, name="amgcl_params_destroy")
             use iso_c_binding
-            integer (c_size_t), value :: prm
+            integer (c_size_t), intent(in), value :: prm
         end subroutine
 
         integer(c_size_t) &
         function amgcl_solver_create (n, ptr, col, val, prm) bind (C, name="amgcl_solver_create_f")
             use iso_c_binding
-            integer (c_int),    value :: n
-            integer (c_int)           :: ptr(*)
-            integer (c_int)           :: col(*)
-            real    (c_double)        :: val(*)
-            integer (c_size_t), value :: prm
+            integer (c_int),    intent(in), value :: n
+            integer (c_int),    intent(in)        :: ptr(*)
+            integer (c_int),    intent(in)        :: col(*)
+            real    (c_double), intent(in)        :: val(*)
+            integer (c_size_t), intent(in), value :: prm
         end function
 
         type(conv_info) &
         function amgcl_solver_solve(solver, rhs, x) bind (C, name="amgcl_solver_solve")
             use iso_c_binding
-            integer (c_size_t), value :: solver
-            real    (c_double)        :: rhs(*)
-            real    (c_double)        :: x(*)
+            integer (c_size_t), intent(in), value :: solver
+            real    (c_double), intent(in)        :: rhs(*)
+            real    (c_double), intent(inout)     :: x(*)
 
             type, bind(C) :: conv_info
                 integer (c_int)    :: iterations;
@@ -63,12 +63,12 @@ module amgcl
 
         subroutine amgcl_solver_report(solver) bind(C, name="amgcl_solver_report")
             use iso_c_binding
-            integer (c_size_t), value :: solver
+            integer (c_size_t), intent(in), value :: solver
         end subroutine
 
         subroutine amgcl_solver_destroy(solver) bind(C, name="amgcl_solver_destroy")
             use iso_c_binding
-            integer (c_size_t), value :: solver
+            integer (c_size_t), intent(in), value :: solver
         end subroutine
     end interface
 end module
