5639e39ac301b0e151276ac63bc73b796e416b2d
Denis Demidov <dennis.demidov@gmail.com>
2015-12-25 22:40:50 +0300

    Test every supported component in test_complex.cpp

--- tests/test_complex.cpp
+++ tests/test_complex.cpp
@@ -7,11 +7,17 @@
 #include <amgcl/backend/builtin.hpp>
 #include <amgcl/amgcl.hpp>
 #include <amgcl/make_solver.hpp>
+
 #include <amgcl/coarsening/smoothed_aggregation.hpp>
+
 #include <amgcl/relaxation/gauss_seidel.hpp>
 #include <amgcl/relaxation/spai0.hpp>
+
+#include <amgcl/solver/cg.hpp>
 #include <amgcl/solver/bicgstab.hpp>
 #include <amgcl/solver/bicgstabl.hpp>
+#include <amgcl/solver/gmres.hpp>
+
 #include <amgcl/adapter/crs_tuple.hpp>
 #include <amgcl/adapter/complex.hpp>
 #include <amgcl/profiler.hpp>
@@ -24,28 +30,26 @@ namespace amgcl {
 
 BOOST_AUTO_TEST_SUITE( test_complex )
 
-BOOST_AUTO_TEST_CASE(complex_matrix)
+typedef std::complex<double> complex;
+typedef amgcl::backend::builtin<complex> Backend;
+
+template <
+    class Coarsening,
+    template <class> class Relaxation,
+    class Solver
+    >
+void test_complex_matrix(
+        size_t n,
+        std::vector<int>     ptr,
+        std::vector<int>     col,
+        std::vector<complex> val,
+        std::vector<complex> rhs
+        )
 {
-    typedef std::complex<double> complex;
-
-    std::vector<int>     ptr;
-    std::vector<int>     col;
-    std::vector<complex> val;
-    std::vector<complex> rhs;
-
-    size_t n = sample_problem(32, val, col, ptr, rhs);
-
     std::vector<complex> x(n, amgcl::math::zero<complex>());
 
-    typedef amgcl::backend::builtin<complex> Backend;
-
     amgcl::make_solver<
-        amgcl::amg<
-            Backend,
-            amgcl::coarsening::smoothed_aggregation,
-            amgcl::relaxation::gauss_seidel
-            >,
-        amgcl::solver::bicgstabl<Backend>
+        amgcl::amg<Backend, Coarsening, Relaxation>, Solver
         > solve( boost::tie(n, ptr, col, val) );
 
     std::cout << solve.precond() << std::endl;
@@ -61,6 +65,50 @@ BOOST_AUTO_TEST_CASE(complex_matrix)
         << "iters: " << iters << std::endl
         << "resid: " << resid << std::endl
         ;
+}
+
+BOOST_AUTO_TEST_CASE(complex_matrix)
+{
+
+    std::vector<int>     ptr;
+    std::vector<int>     col;
+    std::vector<complex> val;
+    std::vector<complex> rhs;
+
+    size_t n = sample_problem(32, val, col, ptr, rhs);
+
+    // Solvers
+    test_complex_matrix<
+        amgcl::coarsening::smoothed_aggregation,
+        amgcl::relaxation::gauss_seidel,
+        amgcl::solver::cg<Backend>
+        >(n, ptr, col, val, rhs);
+
+    test_complex_matrix<
+        amgcl::coarsening::smoothed_aggregation,
+        amgcl::relaxation::gauss_seidel,
+        amgcl::solver::bicgstab<Backend>
+        >(n, ptr, col, val, rhs);
+
+    test_complex_matrix<
+        amgcl::coarsening::smoothed_aggregation,
+        amgcl::relaxation::gauss_seidel,
+        amgcl::solver::bicgstabl<Backend>
+        >(n, ptr, col, val, rhs);
+
+    test_complex_matrix<
+        amgcl::coarsening::smoothed_aggregation,
+        amgcl::relaxation::gauss_seidel,
+        amgcl::solver::gmres<Backend>
+        >(n, ptr, col, val, rhs);
+
+    // Relaxations
+    test_complex_matrix<
+        amgcl::coarsening::smoothed_aggregation,
+        amgcl::relaxation::gauss_seidel,
+        amgcl::solver::cg<Backend>
+        >(n, ptr, col, val, rhs);
+
 }
 
 BOOST_AUTO_TEST_CASE(complex_matrix_adapter)
