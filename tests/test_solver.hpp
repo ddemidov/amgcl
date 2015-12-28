0f83072c3fcdf34536d9b79f229e855cbf0e193f
Denis Demidov <dennis.demidov@gmail.com>
2015-12-28 21:14:35 +0300

    Reduce test time

--- tests/test_solver.hpp
+++ tests/test_solver.hpp
@@ -88,21 +88,21 @@ void test_rap(
 template <class Backend>
 void test_backend() {
     amgcl::runtime::coarsening::type coarsening[] = {
-        amgcl::runtime::coarsening::ruge_stuben,
         amgcl::runtime::coarsening::aggregation,
         amgcl::runtime::coarsening::smoothed_aggregation,
-        amgcl::runtime::coarsening::smoothed_aggr_emin
+        amgcl::runtime::coarsening::smoothed_aggr_emin,
+        amgcl::runtime::coarsening::ruge_stuben
     };
 
     amgcl::runtime::relaxation::type relaxation[] = {
+        amgcl::runtime::relaxation::spai0,
+        amgcl::runtime::relaxation::spai1,
+        amgcl::runtime::relaxation::damped_jacobi,
         amgcl::runtime::relaxation::gauss_seidel,
         amgcl::runtime::relaxation::multicolor_gauss_seidel,
         amgcl::runtime::relaxation::ilu0,
         amgcl::runtime::relaxation::parallel_ilu0,
         amgcl::runtime::relaxation::ilut,
-        amgcl::runtime::relaxation::damped_jacobi,
-        amgcl::runtime::relaxation::spai0,
-        amgcl::runtime::relaxation::spai1,
         amgcl::runtime::relaxation::chebyshev
     };
 
@@ -129,24 +129,45 @@ void test_backend() {
     boost::shared_ptr<vector> y = Backend::copy_vector(rhs, prm);
     boost::shared_ptr<vector> x = Backend::create_vector(n, prm);
 
+    // Test solvers
     BOOST_FOREACH(amgcl::runtime::solver::type s, solver) {
-        BOOST_FOREACH(amgcl::runtime::relaxation::type r, relaxation) {
-            std::cout << s << " " << r << std::endl;
-
-            try {
-                test_rap<Backend>(amgcl::adapter::zero_copy(n, ptr.data(), col.data(), val.data()), y, x, s, r);
-            } catch(const std::logic_error&) {}
-
-            BOOST_FOREACH(amgcl::runtime::coarsening::type c, coarsening) {
-                std::cout << s << " " << r << " " << c << std::endl;
-
-                try {
-                    test_solver<Backend>( amgcl::adapter::zero_copy(n, ptr.data(), col.data(), val.data()), y, x, s, r, c);
-                } catch(const std::logic_error&) {}
-            }
-        }
+        std::cout << "Solver: " << s << std::endl;
+        try {
+            test_solver<Backend>(
+                    amgcl::adapter::zero_copy(n, ptr.data(), col.data(), val.data()),
+                    y, x, s, relaxation[0], coarsening[0]
+                    );
+        } catch(const std::logic_error&) {}
     }
 
+    // Test smoothers
+    BOOST_FOREACH(amgcl::runtime::relaxation::type r, relaxation) {
+        std::cout << "Relaxation: " << r << std::endl;
+        try {
+            test_solver<Backend>(
+                    amgcl::adapter::zero_copy(n, ptr.data(), col.data(), val.data()),
+                    y, x, solver[0], r, coarsening[0]);
+        } catch(const std::logic_error&) {}
+
+        try {
+            std::cout << "Relaxation as preconditioner: " << r << std::endl;
+
+            test_rap<Backend>(
+                    amgcl::adapter::zero_copy(n, ptr.data(), col.data(), val.data()),
+                    y, x, solver[0], r);
+        } catch(const std::logic_error&) {}
+    }
+
+    // Test coarsening
+    BOOST_FOREACH(amgcl::runtime::coarsening::type c, coarsening) {
+        std::cout << "Coarsening: " << c << std::endl;
+
+        try {
+            test_solver<Backend>(
+                    amgcl::adapter::zero_copy(n, ptr.data(), col.data(), val.data()),
+                    y, x, solver[0], relaxation[0], c);
+        } catch(const std::logic_error&) {}
+    }
 }
 
 #endif
