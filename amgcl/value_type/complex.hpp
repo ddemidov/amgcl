2a9ac47abb21d0a20769f7901b85a5b8a9f3d930
Denis Demidov <dennis.demidov@gmail.com>
2016-01-11 14:07:55 +0300

    Happy New Year!

--- amgcl/value_type/complex.hpp
+++ amgcl/value_type/complex.hpp
@@ -4,7 +4,7 @@
 /*
 The MIT License
 
-Copyright (c) 2012-2015 Christoph Sohrmann
+Copyright (c) 2015 Christoph Sohrmann
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
