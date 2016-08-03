ee30cb508685557b3d88497bfdfc8e5e45e1d24e
Denis Demidov <dennis.demidov@gmail.com>
2016-08-03 16:23:49 +0300

    Minor optimization for inverse(static_matrix)

--- amgcl/value_type/static_matrix.hpp
+++ amgcl/value_type/static_matrix.hpp
@@ -311,13 +311,14 @@ struct inverse_impl< static_matrix<T, N, N> >
     static static_matrix<T, N, N> get(static_matrix<T, N, N> A) {
         // Perform LU-factorization of A in-place
         for(int k = 0; k < N; ++k) {
-            T d = A(k,k);
+            T d = 1 / A(k,k);
             assert(!math::is_zero(d));
             for(int i = k+1; i < N; ++i) {
-                A(i,k) /= d;
+                A(i,k) *= d;
                 for(int j = k+1; j < N; ++j)
                     A(i,j) -= A(i,k) * A(k,j);
             }
+            A(k,k) = d;
         }
 
         // Invert identity matrix in-place to get the solution.
@@ -335,7 +336,7 @@ struct inverse_impl< static_matrix<T, N, N> >
             for(int i = N; i --> 0; ) {
                 for(int j = i+1; j < N; ++j)
                     y(i,k) -= A(i,j) * y(j,k);
-                y(i,k) /= A(i,i);
+                y(i,k) *= A(i,i);
             }
         }
 
