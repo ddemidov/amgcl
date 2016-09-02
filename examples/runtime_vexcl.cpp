f422865c037ebaee243a40bf213cabff20b7dce6
Denis Demidov <dennis.demidov@gmail.com>
2016-09-02 16:42:57 +0300

    Exposing block solver in examples/runtime_vexcl.cpp
    This is not working at the moment, needs work on vexcl side of things.
    ref #27

--- examples/runtime_vexcl.cpp
+++ examples/runtime_vexcl.cpp
@@ -1,67 +1,262 @@
 #include <iostream>
 #include <string>
 
+#include <boost/scope_exit.hpp>
 #include <boost/program_options.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/foreach.hpp>
+#include <boost/range/iterator_range.hpp>
 
 #include <amgcl/backend/vexcl.hpp>
+#include <amgcl/value_type/static_matrix.hpp>
 #include <amgcl/runtime.hpp>
 #include <amgcl/make_solver.hpp>
+#include <amgcl/adapter/zero_copy.hpp>
+#include <amgcl/adapter/block_matrix.hpp>
 #include <amgcl/adapter/crs_tuple.hpp>
+#include <amgcl/io/mm.hpp>
+#include <amgcl/io/binary.hpp>
 #include <amgcl/profiler.hpp>
 
 #include "sample_problem.hpp"
 
-namespace amgcl {
-    profiler<> prof;
+namespace amgcl { profiler<> prof; }
+using amgcl::prof;
+using amgcl::precondition;
+
+typedef amgcl::scoped_tic< amgcl::profiler<> > scoped_tic;
+
+namespace vex {
+    template <typename T, int N, int M>
+    struct is_cl_native< amgcl::static_matrix<T, N, M> > : std::true_type {};
+
+    template <typename T, int N, int M>
+    struct type_name_impl< amgcl::static_matrix<T, N, M> >
+    {
+        static std::string get() {
+            std::ostringstream s;
+            s << "amgcl_static_matrix<" << type_name<T>() << "," << N << "," << M << ">";
+            return s.str();
+        }
+    };
+
+    template <typename T, int N, int M>
+    struct cl_scalar_of< amgcl::static_matrix<T, N, M> > {
+        typedef T type;
+    };
+} // namespace vex
+
+//---------------------------------------------------------------------------
+template <int B, template <class> class Precond>
+boost::tuple<size_t, double> block_solve(
+        const boost::property_tree::ptree &prm,
+        size_t rows,
+        std::vector<ptrdiff_t> const &ptr,
+        std::vector<ptrdiff_t> const &col,
+        std::vector<double>    const &val,
+        std::vector<double>    const &rhs,
+        std::vector<double>          &x
+        )
+{
+    typedef amgcl::static_matrix<double, B, B> value_type;
+    typedef amgcl::static_matrix<double, B, 1> rhs_type;
+    typedef amgcl::backend::vexcl<value_type> Backend;
+
+    vex::Context ctx(vex::Filter::Env);
+    std::cout << ctx << std::endl;
+
+    typename Backend::params bprm;
+    bprm.q = ctx;
+
+    typedef amgcl::make_solver<
+        Precond<Backend>,
+        amgcl::runtime::iterative_solver<Backend>
+        > Solver;
+
+    prof.tic("setup");
+    Solver solve(amgcl::adapter::block_matrix<B, value_type>(boost::tie(rows, ptr, col, val)), prm, bprm);
+    prof.toc("setup");
+
+    std::cout << solve.precond() << std::endl;
+
+    {
+        scoped_tic t(prof, "solve");
+
+        rhs_type const * fptr = reinterpret_cast<rhs_type const *>(&rhs[0]);
+        rhs_type       * xptr = reinterpret_cast<rhs_type       *>(&x[0]);
+
+        vex::vector<rhs_type> f(ctx, rows, fptr);
+        vex::vector<rhs_type> u(ctx, rows, xptr);
+
+        boost::tuple<size_t, double> r = solve(f, u);
+        vex::copy(u.begin(), u.end(), xptr);
+        return r;
+    }
 }
 
+//---------------------------------------------------------------------------
+template <template <class> class Precond>
+boost::tuple<size_t, double> scalar_solve(
+        const boost::property_tree::ptree &prm,
+        size_t rows,
+        std::vector<ptrdiff_t> const &ptr,
+        std::vector<ptrdiff_t> const &col,
+        std::vector<double>    const &val,
+        std::vector<double>    const &rhs,
+        std::vector<double>          &x
+        )
+{
+    typedef amgcl::backend::vexcl<double> Backend;
+
+    vex::Context ctx(vex::Filter::Env);
+    std::cout << ctx << std::endl;
+
+    typename Backend::params bprm;
+    bprm.q = ctx;
+
+    typedef amgcl::make_solver<
+        Precond<Backend>,
+        amgcl::runtime::iterative_solver<Backend>
+        > Solver;
+
+    prof.tic("setup");
+    Solver solve(amgcl::adapter::zero_copy(rows, &ptr[0], &col[0], &val[0]), prm, bprm);
+    prof.toc("setup");
+
+    std::cout << solve.precond() << std::endl;
+
+    {
+        scoped_tic t(prof, "solve");
+
+        vex::vector<double> f(ctx, rhs);
+        vex::vector<double> u(ctx, x);
+
+        boost::tuple<size_t, double> r = solve(f, u);
+        vex::copy(u, x);
+        return r;
+    }
+}
+
+//---------------------------------------------------------------------------
+template <template <class> class Precond>
+boost::tuple<size_t, double> solve(
+        const boost::property_tree::ptree &prm,
+        size_t rows,
+        std::vector<ptrdiff_t> const &ptr,
+        std::vector<ptrdiff_t> const &col,
+        std::vector<double>    const &val,
+        std::vector<double>    const &rhs,
+        std::vector<double>          &x,
+        int block_size
+        )
+{
+    switch (block_size) {
+        case 1:
+            return scalar_solve<Precond>(prm, rows, ptr, col, val, rhs, x);
+#if 0
+        case 2:
+            return block_solve<2, Precond>(prm, rows, ptr, col, val, rhs, x);
+        case 3:
+            return block_solve<3, Precond>(prm, rows, ptr, col, val, rhs, x);
+        case 4:
+            return block_solve<4, Precond>(prm, rows, ptr, col, val, rhs, x);
+        case 5:
+            return block_solve<5, Precond>(prm, rows, ptr, col, val, rhs, x);
+        case 6:
+            return block_solve<6, Precond>(prm, rows, ptr, col, val, rhs, x);
+#endif
+        default:
+            precondition(false, "Unsupported block size");
+            return boost::make_tuple(0, 0.0);
+    }
+}
+
+//---------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
-    using amgcl::prof;
-
-    // Read configuration from command line
-    int m = 32;
-    bool just_relax = false;
-    amgcl::runtime::coarsening::type coarsening = amgcl::runtime::coarsening::smoothed_aggregation;
-    amgcl::runtime::relaxation::type relaxation = amgcl::runtime::relaxation::spai0;
-    amgcl::runtime::solver::type     solver     = amgcl::runtime::solver::bicgstab;
-    std::string parameter_file;
-
     namespace po = boost::program_options;
+    namespace io = amgcl::io;
+
+    using amgcl::prof;
+    using std::vector;
+    using std::string;
+
     po::options_description desc("Options");
 
     desc.add_options()
-        ("help,h", "show help")
+        ("help,h", "Show this help.")
+        ("prm-file,P",
+         po::value<string>(),
+         "Parameter file in json format. "
+        )
+        (
+         "prm,p",
+         po::value< vector<string> >(),
+         "Parameters specified as name=value pairs. "
+         "May be provided multiple times. Examples:\n"
+         "  -p solver.tol=1e-3\n"
+         "  -p precond.coarse_enough=300"
+        )
+        ("matrix,A",
+         po::value<string>(),
+         "System matrix in the MatrixMarket format. "
+         "When not specified, solves a Poisson problem in 3D unit cube. "
+        )
+        (
+         "rhs,f",
+         po::value<string>(),
+         "The RHS vector in the MatrixMarket format. "
+         "When omitted, a vector of ones is used by default. "
+         "Should only be provided together with a system matrix. "
+        )
+        (
+         "null,N",
+         po::value<string>(),
+         "The near null-space vectors in the MatrixMarket format. "
+         "Should be a dense matrix of size N*M, where N is the number of "
+         "unknowns, and M is the number of null-space vectors. "
+         "Should only be provided together with a system matrix. "
+        )
+        (
+         "binary,B",
+         po::bool_switch()->default_value(false),
+         "When specified, treat input files as binary instead of as MatrixMarket. "
+         "It is assumed the files were converted to binary format with mm2bin utility. "
+        )
+        (
+         "block-size,b",
+         po::value<int>()->default_value(1),
+         "The block size of the system matrix. "
+         "When specified, the system matrix is assumed to have block-wise structure. "
+         "This usually is the case for problems in elasticity, structural mechanics, "
+         "for coupled systems of PDE (such as Navier-Stokes equations), etc. "
+         "Valid choices are 2, 3, 4, and 6."
+        )
         (
          "size,n",
-         po::value<int>(&m)->default_value(m),
-         "domain size"
+         po::value<int>()->default_value(32),
+         "The size of the Poisson problem to solve when no system matrix is given. "
+         "Specified as number of grid nodes along each dimension of a unit cube. "
+         "The resulting system will have n*n*n unknowns. "
         )
         (
-         "coarsening,c",
-         po::value<amgcl::runtime::coarsening::type>(&coarsening)->default_value(coarsening),
-         "ruge_stuben, aggregation, smoothed_aggregation, smoothed_aggr_emin"
+         "single-level,1",
+         po::bool_switch()->default_value(false),
+         "When specified, the AMG hierarchy is not constructed. "
+         "Instead, the problem is solved using a single-level smoother as preconditioner. "
         )
         (
-         "relaxation,r",
-         po::value<amgcl::runtime::relaxation::type>(&relaxation)->default_value(relaxation),
-         "parallel_ilu0, damped_jacobi, spai0, chebyshev"
+         "initial,x",
+         po::value<double>()->default_value(0),
+         "Value to use as initial approximation. "
         )
         (
-         "just-relax,0",
-         po::bool_switch(&just_relax),
-         "Do not create AMG hierarchy, use relaxation as preconditioner"
-        )
-        (
-         "solver,s",
-         po::value<amgcl::runtime::solver::type>(&solver)->default_value(solver),
-         "cg, bicgstab, bicgstabl, gmres"
-        )
-        (
-         "params,p",
-         po::value<std::string>(&parameter_file),
-         "parameter file in json format"
+         "output,o",
+         po::value<string>(),
+         "Output file. Will be saved in the MatrixMarket format. "
+         "When omitted, the solution is not saved. "
         )
         ;
 
@@ -75,69 +270,107 @@ int main(int argc, char *argv[]) {
     }
 
     boost::property_tree::ptree prm;
-    if (vm.count("params")) read_json(parameter_file, prm);
-
-    vex::Context ctx(vex::Filter::Env && vex::Filter::DoublePrecision);
-    std::cout << ctx << std::endl;
-
-    // Assemble problem
-    prof.tic("assemble");
-    std::vector<int>    ptr;
-    std::vector<int>    col;
-    std::vector<double> val;
-    std::vector<double> rhs;
-
-    int n = sample_problem(m, val, col, ptr, rhs);
-    prof.toc("assemble");
-
-    // Solve the problem
-    vex::vector<double> f(ctx, rhs);
-    vex::vector<double> x(ctx, n);
-    x = 0;
-
-    size_t iters;
-    double resid;
-
-    typedef amgcl::backend::vexcl<double> Backend;
-    typename Backend::params bprm;
-    bprm.q = ctx;
-
-    prm.put("solver.type", solver);
-
-    if (just_relax) {
-        prm.put("precond.type", relaxation);
-
-        prof.tic("setup");
-        amgcl::make_solver<
-            amgcl::runtime::relaxation::as_preconditioner<Backend>,
-            amgcl::runtime::iterative_solver<Backend>
-            > solve(boost::tie(n, ptr, col, val), prm, bprm);
-        prof.toc("setup");
-
-        std::cout << "Using relaxation as preconditioner" << std::endl;
-
-        prof.tic("solve");
-        boost::tie(iters, resid) = solve(f, x);
-        prof.toc("solve");
-    } else {
-        prm.put("precond.coarsening.type", coarsening);
-        prm.put("precond.relax.type", relaxation);
-
-        prof.tic("setup");
-        amgcl::make_solver<
-            amgcl::runtime::amg<Backend>,
-            amgcl::runtime::iterative_solver<Backend>
-                > solve(boost::tie(n, ptr, col, val), prm, bprm);
-        prof.toc("setup");
-
-        std::cout << solve.precond() << std::endl;
-
-        prof.tic("solve");
-        boost::tie(iters, resid) = solve(f, x);
-        prof.toc("solve");
+    if (vm.count("prm-file")) {
+        read_json(vm["prm-file"].as<string>(), prm);
     }
 
-    std::cout << "Iterations: " << iters << std::endl
-              << "Error:      " << resid << std::endl
-              << std::endl      << prof  << std::endl;
+    if (vm.count("prm")) {
+        BOOST_FOREACH(string pair, vm["prm"].as<vector<string> >())
+        {
+            using namespace boost::algorithm;
+            vector<string> key_val;
+            split(key_val, pair, is_any_of("="));
+            if (key_val.size() != 2) throw po::invalid_option_value(
+                    "Parameters specified with -p option "
+                    "should have name=value format");
+
+            prm.put(key_val[0], key_val[1]);
+        }
+    }
+
+    size_t rows;
+    vector<ptrdiff_t> ptr, col;
+    vector<double> val, rhs, null, x;
+
+    if (vm.count("matrix")) {
+        scoped_tic t(prof, "reading");
+
+        string Afile  = vm["matrix"].as<string>();
+        bool   binary = vm["binary"].as<bool>();
+
+        if (binary) {
+            io::read_crs(Afile, rows, ptr, col, val);
+        } else {
+            size_t cols;
+            boost::tie(rows, cols) = io::mm_reader(Afile)(ptr, col, val);
+            precondition(rows == cols, "Non-square system matrix");
+        }
+
+        if (vm.count("rhs")) {
+            string bfile = vm["rhs"].as<string>();
+
+            size_t n, m;
+
+            if (binary) {
+                io::read_dense(bfile, n, m, rhs);
+            } else {
+                boost::tie(n, m) = io::mm_reader(bfile)(rhs);
+            }
+
+            precondition(n == rows && m == 1, "The RHS vector has wrong size");
+        } else {
+            rhs.resize(rows, 1.0);
+        }
+
+        if (vm.count("null")) {
+            string nfile = vm["null"].as<string>();
+
+            size_t m, nv;
+
+            if (binary) {
+                io::read_dense(nfile, m, nv, null);
+            } else {
+                boost::tie(m, nv) = io::mm_reader(nfile)(null);
+            }
+
+            precondition(m == rows, "Near null-space vectors have wrong size");
+
+            prm.put("precond.coarsening.nullspace.cols", nv);
+            prm.put("precond.coarsening.nullspace.rows", rows);
+            prm.put("precond.coarsening.nullspace.B",    &null[0]);
+        }
+    } else {
+        scoped_tic t(prof, "assembling");
+        rows = sample_problem(vm["size"].as<int>(), val, col, ptr, rhs);
+    }
+
+    x.resize(rows, vm["initial"].as<double>());
+
+    size_t iters;
+    double error;
+
+    int block_size    = vm["block-size"].as<int>();
+    bool single_level = vm["single-level"].as<bool>();
+
+    if (single_level) {
+        boost::tie(iters, error) = solve<amgcl::runtime::relaxation::as_preconditioner>(
+                prm, rows, ptr, col, val, rhs, x, block_size);
+    } else {
+        boost::tie(iters, error) = solve<amgcl::runtime::amg>(
+                prm, rows, ptr, col, val, rhs, x, block_size);
+    }
+
+    double norm_rhs = sqrt(amgcl::backend::inner_product(rhs, rhs));
+    amgcl::backend::spmv(-1, boost::tie(rows, ptr, col, val), x, 1, rhs);
+    double resid = sqrt(amgcl::backend::inner_product(rhs, rhs)) / norm_rhs;
+
+    if (vm.count("output")) {
+        scoped_tic t(prof, "write");
+        amgcl::io::mm_write(vm["output"].as<string>(), &x[0], x.size());
+    }
+
+    std::cout << "Iterations:     " << iters << std::endl
+              << "Reported error: " << error << std::endl
+              << "Real error:     " << resid << std::endl
+              << prof << std::endl;
 }
