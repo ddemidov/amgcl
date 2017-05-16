158b44ba78c5bb5926e249bcf42bb043681a4d8b
Denis Demidov <dennis.demidov@gmail.com>
2017-05-16 11:28:47 +0300

    Proper implementation of multicolor Gauss-Seidel smoother

--- amgcl/relaxation/multicolor_gauss_seidel.hpp
+++ amgcl/relaxation/multicolor_gauss_seidel.hpp
@@ -34,85 +34,10 @@ THE SOFTWARE.
 #include <algorithm>
 #include <numeric>
 
-#include <boost/graph/sequential_vertex_coloring.hpp>
-#include <boost/iterator/counting_iterator.hpp>
-
 #include <amgcl/backend/interface.hpp>
 #include <amgcl/util.hpp>
 
 namespace amgcl {
-
-namespace detail {
-
-//---------------------------------------------------------------------------
-// Adapt backend::crs<> for use with Boost Graph Library.
-//---------------------------------------------------------------------------
-template <class Matrix>
-struct graph {
-    const Matrix &A;
-
-    typedef typename Matrix::col_type vertex_descriptor;
-    typedef typename Matrix::ptr_type edge_descriptor;
-    typedef boost::directed_tag directed_category;
-    typedef boost::allow_parallel_edge_tag edge_parallel_category;
-
-    class traversal_category:
-        public boost::incidence_graph_tag,
-        public boost::adjacency_graph_tag,
-        public boost::vertex_list_graph_tag,
-        public boost::edge_list_graph_tag
-    {};
-
-    typedef boost::counting_iterator<vertex_descriptor> vertex_iterator;
-    typedef vertex_descriptor vertices_size_type;
-
-    typedef const vertex_descriptor* adjacency_iterator;
-
-    static vertex_descriptor null_vertex() { return -1; }
-
-    graph(const Matrix &A) : A(A) {}
-};
-
-template <class Matrix>
-std::pair<
-    typename graph<Matrix>::vertex_iterator,
-    typename graph<Matrix>::vertex_iterator
-    >
-vertices(const graph<Matrix> &G) {
-    return std::make_pair(
-            boost::counting_iterator<typename graph<Matrix>::vertex_descriptor>(0),
-            boost::counting_iterator<typename graph<Matrix>::vertex_descriptor>(backend::rows(G.A))
-            );
-}
-
-template <class Matrix>
-std::pair<
-    typename graph<Matrix>::adjacency_iterator,
-    typename graph<Matrix>::adjacency_iterator
-    >
-adjacent_vertices(ptrdiff_t v, const graph<Matrix> &G) {
-    typename Matrix::ptr_type row_beg = G.A.ptr[v];
-    typename Matrix::ptr_type row_end = G.A.ptr[v + 1];
-
-    return std::make_pair(
-            G.A.col + row_beg,
-            G.A.col + row_end
-            );
-}
-
-template <class Matrix>
-typename graph<Matrix>::vertices_size_type
-num_vertices(const graph<Matrix> &G) {
-    return backend::rows(G.A);
-}
-
-template <class Matrix>
-graph<Matrix> as_graph(const Matrix &A) {
-    return graph<Matrix>(A);
-}
-
-} // namespace detail
-
 namespace relaxation {
 
 template <class Backend>
@@ -124,83 +49,224 @@ struct multicolor_gauss_seidel {
             const Matrix &A,
             const params&,
             const typename Backend::params&
-            ) : order( backend::rows(A) )
+            )
+        : forward(A), backward(A)
     {
-        const size_t n = backend::rows(A);
-
-        std::vector<int> color(n);
-        num_colors = boost::sequential_vertex_coloring(amgcl::detail::as_graph(A), &color[0]);
-
-        ptr.resize(num_colors + 1, 0);
-
-        for(size_t i = 0; i < n; ++i) {
-            ++ptr[ color[i] + 1];
-            order[i] = i;
-        }
-
-        std::stable_sort(order.begin(), order.end(), order_by(color));
-        std::partial_sum(ptr.begin(), ptr.end(), ptr.begin());
     }
 
     template <class Matrix, class VecRHS, class VecX, class VecTMP>
-    void apply_pre(const Matrix &A, const VecRHS &rhs, VecX &x, VecTMP&, const params&) const
+    void apply_pre(const Matrix&, const VecRHS &rhs, VecX &x, VecTMP&, const params&) const
     {
-        for(int c = 0; c < num_colors; ++c) iterate(A, rhs, x, c);
+        forward.sweep(rhs, x);
     }
 
     template <class Matrix, class VecRHS, class VecX, class VecTMP>
-    void apply_post(const Matrix &A, const VecRHS &rhs, VecX &x, VecTMP&, const params&) const
+    void apply_post(const Matrix&, const VecRHS &rhs, VecX &x, VecTMP&, const params&) const
     {
-        for(int c = num_colors; c --> 0; ) iterate(A, rhs, x, c);
+        backward.sweep(rhs, x);
     }
 
     template <class Matrix, class VecRHS, class VecX>
-    void apply(const Matrix &A, const VecRHS &rhs, VecX &x, const params&) const
+    void apply(const Matrix&, const VecRHS &rhs, VecX &x, const params&) const
     {
         backend::clear(x);
-        for(int c = 0; c < num_colors; ++c) iterate(A, rhs, x, c);
-        for(int c = num_colors; c --> 0; )  iterate(A, rhs, x, c);
+        forward.sweep(rhs, x);
+        backward.sweep(rhs, x);
     }
 
     private:
-        struct order_by {
-            const std::vector<int> &ref;
+        static int num_threads() {
+#ifdef _OPENMP
+            return omp_get_max_threads();
+#else
+            return 1;
+#endif
+        }
 
-            order_by(const std::vector<int> &ref) : ref(ref) {}
+        static int thread_id() {
+#ifdef _OPENMP
+            return omp_get_thread_num();
+#else
+            return 0;
+#endif
+        }
 
-            bool operator()(ptrdiff_t i, ptrdiff_t j) const {
-                return ref[i] < ref[j];
+        template <bool forward>
+        struct gs_sweep {
+            typedef typename Backend::value_type value_type;
+            typedef typename Backend::rhs_type   rhs_type;
+
+            struct task {
+                ptrdiff_t beg, end;
+                task(ptrdiff_t beg, ptrdiff_t end) : beg(beg), end(end) {}
+            };
+
+            int nthreads;
+
+            // thread-specific storage:
+            std::vector< std::vector<task>       > tasks;
+            std::vector< std::vector<ptrdiff_t>  > ptr;
+            std::vector< std::vector<ptrdiff_t>  > col;
+            std::vector< std::vector<value_type> > val;
+            std::vector< std::vector<ptrdiff_t>  > ord;
+
+            template <class Matrix>
+            gs_sweep(const Matrix &A)
+                : nthreads(num_threads()), tasks(nthreads),
+                  ptr(nthreads), col(nthreads), val(nthreads), ord(nthreads)
+            {
+                typedef typename backend::row_iterator<Matrix>::type row_iterator;
+
+                ptrdiff_t n    = backend::rows(A);
+                ptrdiff_t nlev = 0;
+
+                std::vector<ptrdiff_t> level(n, 0);
+                std::vector<ptrdiff_t> order(n, 0);
+
+                // 1. split rows into levels.
+                ptrdiff_t beg = forward ? 0 : n-1;
+                ptrdiff_t end = forward ? n :  -1;
+                ptrdiff_t inc = forward ? 1 :  -1;
+
+                for(ptrdiff_t i = beg; i != end; i += inc) {
+                    ptrdiff_t l = level[i];
+
+                    for(row_iterator a = row_begin(A, i); a; ++a) {
+                        ptrdiff_t c = a.col();
+
+                        if (forward) {
+                            if (c >= i) continue;
+                        } else {
+                            if (c <= i) continue;
+                        }
+
+                        l = std::max(l, level[c]+1);
+                    }
+
+                    level[i] = l;
+                    nlev = std::max(nlev, l+1);
+                }
+
+
+                // 2. reorder matrix rows.
+                std::vector<ptrdiff_t> start(nlev+1, 0);
+
+                for(ptrdiff_t i = 0; i < n; ++i)
+                    ++start[level[i]+1];
+
+                std::partial_sum(start.begin(), start.end(), start.begin());
+
+                for(ptrdiff_t i = 0; i < n; ++i)
+                    order[start[level[i]]++] = i;
+
+                std::rotate(start.begin(), start.end() - 1, start.end());
+                start[0] = 0;
+
+
+                // 3. Organize matrix rows into tasks.
+                //    Each level is split into nthreads tasks.
+                std::vector<ptrdiff_t> thread_rows(nthreads, 0);
+                std::vector<ptrdiff_t> thread_cols(nthreads, 0);
+
+#pragma omp parallel
+                {
+                    int tid = thread_id();
+                    tasks[tid].reserve(nlev);
+
+                    for(ptrdiff_t lev = 0; lev < nlev; ++lev) {
+                        // split each level into tasks.
+                        ptrdiff_t lev_size = start[lev+1] - start[lev];
+                        ptrdiff_t chunk_size = (lev_size + nthreads - 1) / nthreads;
+
+                        ptrdiff_t beg = std::min(tid * chunk_size, lev_size);
+                        ptrdiff_t end = std::min(beg + chunk_size, lev_size);
+
+                        beg += start[lev];
+                        end += start[lev];
+
+                        tasks[tid].push_back(task(beg, end));
+
+                        // count rows and nonzeros in the current task
+                        thread_rows[tid] += end - beg;
+                        for(ptrdiff_t i = beg; i < end; ++i) {
+                            ptrdiff_t j = order[i];
+                            thread_cols[tid] += row_nonzeros(A, j);
+                        }
+                    }
+                }
+
+                // 4. reorganize matrix data for better cache and NUMA locality.
+#pragma omp parallel
+                {
+                    int tid = thread_id();
+
+                    col[tid].reserve(thread_cols[tid]);
+                    val[tid].reserve(thread_cols[tid]);
+                    ord[tid].reserve(thread_rows[tid]);
+                    ptr[tid].reserve(thread_rows[tid] + 1);
+                    ptr[tid].push_back(0);
+
+                    BOOST_FOREACH(task &t, tasks[tid]) {
+                        ptrdiff_t loc_beg = ptr[tid].size() - 1;
+                        ptrdiff_t loc_end = loc_beg;
+
+                        for(ptrdiff_t r = t.beg; r < t.end; ++r, ++loc_end) {
+                            ptrdiff_t i = order[r];
+
+                            ord[tid].push_back(i);
+
+                            for(row_iterator a = row_begin(A, i); a; ++a) {
+                                col[tid].push_back(a.col());
+                                val[tid].push_back(a.value());
+                            }
+
+                            ptr[tid].push_back(col[tid].size());
+                        }
+
+                        t.beg = loc_beg;
+                        t.end = loc_end;
+                    }
+                }
+            }
+
+            template <class Vector1, class Vector2>
+            void sweep(const Vector1 &rhs, Vector2 &x) const {
+#pragma omp parallel
+                {
+                    int tid = thread_id();
+
+                    BOOST_FOREACH(const task &t, tasks[tid]) {
+                        for(ptrdiff_t r = t.beg; r < t.end; ++r) {
+                            ptrdiff_t i   = ord[tid][r];
+                            ptrdiff_t beg = ptr[tid][r];
+                            ptrdiff_t end = ptr[tid][r+1];
+
+                            rhs_type   X = rhs[i];
+                            value_type D = math::identity<value_type>();
+
+                            for(ptrdiff_t j = beg; j < end; ++j) {
+                                ptrdiff_t  c = col[tid][j];
+                                value_type v = val[tid][j];
+
+                                if (c == i)
+                                    D = v;
+                                else
+                                    X -= v * x[c];
+                            }
+
+                            x[i] = math::inverse(D) * X;
+                        }
+
+                        // each task corresponds to a level, so we need
+                        // to synchronize across threads at this point:
+#pragma omp barrier
+                    }
+                }
             }
         };
 
-        int num_colors;
-        std::vector<ptrdiff_t> order, ptr;
-
-        template <class Matrix, class VectorRHS, class VectorX>
-        void iterate(
-                const Matrix &A, const VectorRHS &rhs, VectorX &x, int c
-                ) const
-        {
-            typedef typename backend::row_iterator<Matrix>::type row_iterator;
-            typedef typename backend::value_type<Matrix>::type    val_type;
-            typedef typename backend::value_type<VectorRHS>::type rhs_type;
-
-#pragma omp parallel for
-            for(ptrdiff_t j = ptr[c]; j < ptr[c+1]; ++j) {
-                ptrdiff_t i = order[j];
-
-                rhs_type temp = rhs[i];
-                val_type diag = math::identity<val_type>();
-                for (row_iterator a = backend::row_begin(A, i); a; ++a) {
-                    if (a.col() == i)
-                        diag = a.value();
-                    else
-                        temp -= a.value() * x[a.col()];
-                }
-
-                x[i] = math::inverse(diag) * temp;
-            }
-        }
+        gs_sweep<true > forward;
+        gs_sweep<false> backward;
 };
 
 } // namespace relaxation
