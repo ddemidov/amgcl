ddemidov/amgcl

commit 1013a7c7edb530dc5ef92a76d553b3fca467b0a1

Fix a couple of corner cases in ruge_stuben coarsening

amgcl/coarsening/ruge_stuben.hpp
@@ -230,7 +230,7 @@ struct ruge_stuben {
                 Val  v = A.val[j];
 
                 if (!S.val[j] || cf[c] != 'C') continue;
-                if (prm.do_trunc && Amin[i] < v && v < Amax[i]) continue;
+                if (prm.do_trunc && Amin[i] >= v && v <= Amax[i]) continue;
 
                 P->col[row_head] = cidx[c];
                 P->val[row_head] = (v < zero ? alpha : beta) * v;
@@ -286,6 +286,8 @@ struct ruge_stuben {
 
 #pragma omp parallel for
             for(ptrdiff_t i = 0; i < static_cast<ptrdiff_t>(n); ++i) {
+                S.ptr[i+1] = 0;
+
                 Val a_min = math::zero<Val>();
 
                 for(row_iterator a = row_begin(A, i); a; ++a)
@@ -300,8 +302,6 @@ struct ruge_stuben {
 
                 for(Ptr j = A.ptr[i], e = A.ptr[i + 1]; j < e; ++j)
                     S.val[j] = (A.col[j] != i && A.val[j] < a_min);
-
-                S.ptr[i+1] = 0;
             }
 
             // Transposition of S:

commit 7184bbaca4f46d431e2af36beabd60420611af35

Fix a bug in ruge_stuben introduced in 1013a7c7

amgcl/coarsening/ruge_stuben.hpp
@@ -230,7 +230,7 @@ struct ruge_stuben {
                 Val  v = A.val[j];
 
                 if (!S.val[j] || cf[c] != 'C') continue;
-                if (prm.do_trunc && Amin[i] >= v && v <= Amax[i]) continue;
+                if (prm.do_trunc && Amin[i] <= v && v <= Amax[i]) continue;
 
                 P->col[row_head] = cidx[c];
                 P->val[row_head] = (v < zero ? alpha : beta) * v;