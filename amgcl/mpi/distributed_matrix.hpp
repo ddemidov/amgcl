154b6c6e2e480b6b181afe8b012fb4199a678a9f
Denis Demidov <dennis.demidov@gmail.com>
2018-05-23 19:26:44 +0300

    Avoid hops over shared pointers

--- amgcl/mpi/distributed_matrix.hpp
+++ amgcl/mpi/distributed_matrix.hpp
@@ -329,8 +329,11 @@ class distributed_matrix {
             a_loc = boost::make_shared<build_matrix>();
             a_rem = boost::make_shared<build_matrix>();
 
-            a_loc->set_size(n_loc_rows, n_loc_cols, true);
-            a_rem->set_size(n_loc_rows, 0, true);
+            build_matrix &A_loc = *a_loc;
+            build_matrix &A_rem = *a_rem;
+
+            A_loc.set_size(n_loc_rows, n_loc_cols, true);
+            A_rem.set_size(n_loc_rows, 0, true);
 
 #pragma omp parallel for
             for(ptrdiff_t i = 0; i < n_loc_rows; ++i) {
@@ -338,31 +341,31 @@ class distributed_matrix {
                     ptrdiff_t c = a.col();
 
                     if (loc_beg <= c && c < loc_end)
-                        ++a_loc->ptr[i + 1];
+                        ++A_loc.ptr[i + 1];
                     else
-                        ++a_rem->ptr[i + 1];
+                        ++A_rem.ptr[i + 1];
                 }
             }
 
-            a_loc->set_nonzeros(a_loc->scan_row_sizes());
-            a_rem->set_nonzeros(a_rem->scan_row_sizes());
+            A_loc.set_nonzeros(A_loc.scan_row_sizes());
+            A_rem.set_nonzeros(A_rem.scan_row_sizes());
 
 #pragma omp parallel for
             for(ptrdiff_t i = 0; i < n_loc_rows; ++i) {
-                ptrdiff_t loc_head = a_loc->ptr[i];
-                ptrdiff_t rem_head = a_rem->ptr[i];
+                ptrdiff_t loc_head = A_loc.ptr[i];
+                ptrdiff_t rem_head = A_rem.ptr[i];
 
                 for(row_iterator a = backend::row_begin(A, i); a; ++a) {
                     ptrdiff_t  c = a.col();
                     value_type v = a.value();
 
                     if (loc_beg <= c && c < loc_end) {
-                        a_loc->col[loc_head] = c - loc_beg;
-                        a_loc->val[loc_head] = v;
+                        A_loc.col[loc_head] = c - loc_beg;
+                        A_loc.val[loc_head] = v;
                         ++loc_head;
                     } else {
-                        a_rem->col[rem_head] = c;
-                        a_rem->val[rem_head] = v;
+                        A_rem.col[rem_head] = c;
+                        A_rem.val[rem_head] = v;
                         ++rem_head;
                     }
                 }
@@ -526,7 +529,7 @@ transpose(const distributed_matrix<Backend, Local, Remote> &A) {
 
     // Our transposed remote part becomes remote part of someone else,
     // and the other way around.
-    boost::shared_ptr<build_matrix> t_rem;
+    boost::shared_ptr<build_matrix> t_ptr;
     {
         std::vector<ptrdiff_t> tmp_col(A.a_rem->col, A.a_rem->col + A.a_rem->nnz);
         C.renumber(tmp_col.size(), &tmp_col[0]);
@@ -534,21 +537,22 @@ transpose(const distributed_matrix<Backend, Local, Remote> &A) {
         ptrdiff_t *a_rem_col = &tmp_col[0];
         std::swap(a_rem_col, A.a_rem->col);
 
-        t_rem = backend::transpose(*A.a_rem);
+        t_ptr = backend::transpose(*A.a_rem);
 
         std::swap(a_rem_col, A.a_rem->col);
     }
+    build_matrix &t_rem = *t_ptr;
 
     // Shift to global numbering:
     std::vector<ptrdiff_t> domain = mpi::exclusive_sum(comm, ncols);
     ptrdiff_t loc_beg = domain[comm.rank];
-    for(size_t i = 0; i < t_rem->nnz; ++i)
-        t_rem->col[i] += loc_beg;
+    for(size_t i = 0; i < t_rem.nnz; ++i)
+        t_rem.col[i] += loc_beg;
 
     // Shift from row pointers to row sizes:
-    std::vector<ptrdiff_t> row_size(t_rem->nrows);
-    for(size_t i = 0; i < t_rem->nrows; ++i)
-        row_size[i] = t_rem->ptr[i+1] - t_rem->ptr[i];
+    std::vector<ptrdiff_t> row_size(t_rem.nrows);
+    for(size_t i = 0; i < t_rem.nrows; ++i)
+        row_size[i] = t_rem.ptr[i+1] - t_rem.ptr[i];
 
     // Sizes of transposed remote blocks:
     // 1. Exchange rem_ptr
@@ -597,26 +601,27 @@ transpose(const distributed_matrix<Backend, Local, Remote> &A) {
         ptrdiff_t rbeg = C.recv.ptr[i];
         ptrdiff_t rend = C.recv.ptr[i + 1];
 
-        ptrdiff_t cbeg = t_rem->ptr[rbeg];
-        ptrdiff_t cend = t_rem->ptr[rend];
+        ptrdiff_t cbeg = t_rem.ptr[rbeg];
+        ptrdiff_t cend = t_rem.ptr[rend];
 
-        MPI_Isend(&t_rem->col[cbeg], cend - cbeg, datatype<ptrdiff_t>(),
+        MPI_Isend(&t_rem.col[cbeg], cend - cbeg, datatype<ptrdiff_t>(),
                 C.recv.nbr[i], tag_col, comm, &send_col_req[i]);
 
-        MPI_Isend(&t_rem->val[cbeg], cend - cbeg, datatype<value_type>(),
+        MPI_Isend(&t_rem.val[cbeg], cend - cbeg, datatype<value_type>(),
                 C.recv.nbr[i], tag_val, comm, &send_val_req[i]);
     }
 
     // 3. While rem_col and rem_val are in flight,
     //    start constructing our remote part:
-    boost::shared_ptr<build_matrix> T_rem = boost::make_shared<build_matrix>();
-    T_rem->set_size(nrows, 0, true);
+    boost::shared_ptr<build_matrix> T_ptr = boost::make_shared<build_matrix>();
+    build_matrix &T_rem = *T_ptr;
+    T_rem.set_size(nrows, 0, true);
 
     for(size_t i = 0; i < C.send.count(); ++i)
-        T_rem->ptr[1 + C.send.col[i]] += rem_ptr[i+1] - rem_ptr[i];
+        T_rem.ptr[1 + C.send.col[i]] += rem_ptr[i+1] - rem_ptr[i];
 
-    T_rem->scan_row_sizes();
-    T_rem->set_nonzeros();
+    T_rem.scan_row_sizes();
+    T_rem.set_nonzeros();
 
     // 4. Finish rem_col and rem_val exchange, and
     //    finish contruction of our remote part.
@@ -627,18 +632,18 @@ transpose(const distributed_matrix<Backend, Local, Remote> &A) {
 
     for(size_t i = 0; i < C.send.count(); ++i) {
         ptrdiff_t row  = C.send.col[i];
-        ptrdiff_t head = T_rem->ptr[row];
+        ptrdiff_t head = T_rem.ptr[row];
 
         for(ptrdiff_t j = rem_ptr[i]; j < rem_ptr[i+1]; ++j, ++head) {
-            T_rem->col[head] = rem_col[j];
-            T_rem->val[head] = rem_val[j];
+            T_rem.col[head] = rem_col[j];
+            T_rem.val[head] = rem_val[j];
         }
 
-        T_rem->ptr[row] = head;
+        T_rem.ptr[row] = head;
     }
 
-    std::rotate(T_rem->ptr, T_rem->ptr + nrows, T_rem->ptr + nrows + 1);
-    T_rem->ptr[0] = 0;
+    std::rotate(T_rem.ptr, T_rem.ptr + nrows, T_rem.ptr + nrows + 1);
+    T_rem.ptr[0] = 0;
 
     AMGCL_TIC("MPI Wait");
     MPI_Waitall(send_cnt_req.size(), &send_cnt_req[0], MPI_STATUSES_IGNORE);
@@ -650,7 +655,7 @@ transpose(const distributed_matrix<Backend, Local, Remote> &A) {
     // TODO: This should work correctly, but the performance may be
     // improved by reusing A's communication pattern:
     return boost::make_shared< distributed_matrix<Backend, Local, Remote> >(
-            comm, backend::transpose(*A.a_loc), T_rem, A.bprm);
+            comm, backend::transpose(*A.a_loc), T_ptr, A.bprm);
 }
 
 template <class Backend, class Local, class Remote>
@@ -811,14 +816,17 @@ product(
     }
 
     // Build the product.
-    boost::shared_ptr<build_matrix> C_loc = boost::make_shared<build_matrix>();
-    boost::shared_ptr<build_matrix> C_rem = boost::make_shared<build_matrix>();
+    boost::shared_ptr<build_matrix> c_loc = boost::make_shared<build_matrix>();
+    boost::shared_ptr<build_matrix> c_rem = boost::make_shared<build_matrix>();
 
-    C_loc->set_size(A_rows, B_cols, false);
-    C_rem->set_size(A_rows, 0,      false);
+    build_matrix &C_loc = *c_loc;
+    build_matrix &C_rem = *c_rem;
 
-    C_loc->ptr[0] = 0;
-    C_rem->ptr[0] = 0;
+    C_loc.set_size(A_rows, B_cols, false);
+    C_rem.set_size(A_rows, 0,      false);
+
+    C_loc.ptr[0] = 0;
+    C_rem.ptr[0] = 0;
 
 #pragma omp parallel
     {
@@ -881,13 +889,13 @@ product(
                 }
             }
 
-            C_loc->ptr[ia + 1] = loc_cols;
-            C_rem->ptr[ia + 1] = rem_cols;
+            C_loc.ptr[ia + 1] = loc_cols;
+            C_rem.ptr[ia + 1] = rem_cols;
         }
     }
 
-    C_loc->set_nonzeros(C_loc->scan_row_sizes(), compute_values);
-    C_rem->set_nonzeros(C_rem->scan_row_sizes(), compute_values);
+    C_loc.set_nonzeros(C_loc.scan_row_sizes(), compute_values);
+    C_rem.set_nonzeros(C_rem.scan_row_sizes(), compute_values);
 
 #pragma omp parallel
     {
@@ -896,8 +904,8 @@ product(
 
 #pragma omp for
         for(ptrdiff_t ia = 0; ia < A_rows; ++ia) {
-            ptrdiff_t loc_beg = C_loc->ptr[ia];
-            ptrdiff_t rem_beg = C_rem->ptr[ia];
+            ptrdiff_t loc_beg = C_loc.ptr[ia];
+            ptrdiff_t rem_beg = C_rem.ptr[ia];
             ptrdiff_t loc_end = loc_beg;
             ptrdiff_t rem_end = rem_beg;
 
@@ -914,13 +922,13 @@ product(
                     if (loc_marker[cb] < loc_beg) {
                         loc_marker[cb] = loc_end;
 
-                        C_loc->col[loc_end] = cb;
+                        C_loc.col[loc_end] = cb;
                         if (compute_values)
-                            C_loc->val[loc_end] = va * vb;
+                            C_loc.val[loc_end] = va * vb;
 
                         ++loc_end;
                     } else if (compute_values) {
-                        C_loc->val[loc_marker[cb]] += va * vb;
+                        C_loc.val[loc_marker[cb]] += va * vb;
                     }
                 }
 
@@ -933,12 +941,12 @@ product(
                     if (rem_marker[cb] < rem_beg) {
                         rem_marker[cb] = rem_end;
 
-                        C_rem->col[rem_end] = gb;
-                        if (compute_values) C_rem->val[rem_end] = va * vb;
+                        C_rem.col[rem_end] = gb;
+                        if (compute_values) C_rem.val[rem_end] = va * vb;
 
                         ++rem_end;
                     } else if (compute_values) {
-                        C_rem->val[rem_marker[cb]] += va * vb;
+                        C_rem.val[rem_marker[cb]] += va * vb;
                     }
                 }
             }
@@ -959,12 +967,12 @@ product(
                         if (loc_marker[cb] < loc_beg) {
                             loc_marker[cb] = loc_end;
 
-                            C_loc->col[loc_end] = cb;
-                            if (compute_values) C_loc->val[loc_end] = va * vb;
+                            C_loc.col[loc_end] = cb;
+                            if (compute_values) C_loc.val[loc_end] = va * vb;
 
                             ++loc_end;
                         } else if (compute_values) {
-                            C_loc->val[loc_marker[cb]] += va * vb;
+                            C_loc.val[loc_marker[cb]] += va * vb;
                         }
                     } else {
                         ptrdiff_t cb = rem_idx[gb];
@@ -972,12 +980,12 @@ product(
                         if (rem_marker[cb] < rem_beg) {
                             rem_marker[cb] = rem_end;
 
-                            C_rem->col[rem_end] = gb;
-                            if (compute_values) C_rem->val[rem_end] = va * vb;
+                            C_rem.col[rem_end] = gb;
+                            if (compute_values) C_rem.val[rem_end] = va * vb;
 
                             ++rem_end;
                         } else if (compute_values) {
-                            C_rem->val[rem_marker[cb]] += va * vb;
+                            C_rem.val[rem_marker[cb]] += va * vb;
                         }
                     }
                 }
@@ -996,7 +1004,7 @@ product(
     // TODO: This should work correctly, but we may have enough information to
     // build C's communication pattern here and save some work:
     return boost::make_shared<distributed_matrix<Backend, Local, Remote> >(comm,
-            C_loc, C_rem, A.bprm);
+            c_loc, c_rem, A.bprm);
 }
 
 template <class Backend, class Local, class Remote>
