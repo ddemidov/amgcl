c8c3cd6f403c4ceb6729286c0c4ea90cbd5ac9b4
Denis Demidov <dennis.demidov@gmail.com>
2018-05-30 10:41:29 +0300

    Improve performance of pointwise aggregation

--- amgcl/coarsening/pointwise_aggregates.hpp
+++ amgcl/coarsening/pointwise_aggregates.hpp
@@ -117,27 +117,34 @@ class pointwise_aggregates {
 
 #pragma omp parallel
                 {
-                    std::vector<ptrdiff_t> marker(Ap.nrows, -1);
+                    std::vector<ptrdiff_t> j(prm.block_size);
+                    std::vector<ptrdiff_t> e(prm.block_size);
 
 #pragma omp for
                     for(ptrdiff_t ip = 0; ip < static_cast<ptrdiff_t>(Ap.nrows); ++ip) {
                         ptrdiff_t ia = ip * prm.block_size;
-                        ptrdiff_t row_beg = Ap.ptr[ip];
-                        ptrdiff_t row_end = row_beg;
 
                         for(unsigned k = 0; k < prm.block_size; ++k, ++ia) {
                             id[ia] = prm.block_size * pw_aggr.id[ip] + k;
 
-                            for(ptrdiff_t ja = A.ptr[ia], ea = A.ptr[ia+1]; ja < ea; ++ja) {
-                                ptrdiff_t cp = A.col[ja] / prm.block_size;
+                            j[k] = A.ptr[ia];
+                            e[k] = A.ptr[ia+1];
+                        }
 
-                                if (marker[cp] < row_beg) {
-                                    marker[cp] = row_end;
-                                    strong_connection[ja] = pw_aggr.strong_connection[row_end];
-                                    ++row_end;
-                                } else {
-                                    strong_connection[ja] = pw_aggr.strong_connection[ marker[cp] ];
-                                }
+                        for(ptrdiff_t jp = Ap.ptr[ip], ep = Ap.ptr[ip+1]; jp < ep; ++jp) {
+                            ptrdiff_t cp = Ap.col[jp];
+                            bool      sp = pw_aggr.strong_connection[jp];
+
+                            ptrdiff_t col_end = (cp + 1) * prm.block_size;
+
+                            for(unsigned k = 0; k < prm.block_size; ++k) {
+                                ptrdiff_t beg = j[k];
+                                ptrdiff_t end = e[k];
+
+                                while(beg < end && A.col[beg] < col_end)
+                                    strong_connection[beg++] = sp;
+
+                                j[k] = beg;
                             }
                         }
                     }
