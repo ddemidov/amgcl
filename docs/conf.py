1619001ff26e024e94e33cddedd120e53ac8bd3f
Denis Demidov <dennis.demidov@gmail.com>
2018-07-31 16:14:44 +0300

    Exclude detail namespaces

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ exhale_args = {
     # TIP: if using the sphinx-bootstrap-theme, you need
     # "treeViewIsBootstrap": True,
     "exhaleExecutesDoxygen": True,
-    "exhaleDoxygenStdin":    "INPUT = ../amgcl"
+    "exhaleDoxygenStdin":    "INPUT = ../amgcl\nEXCLUDE_SYMBOLS = detail"
 }
 
 # Add any paths that contain templates here, relative to this directory.
