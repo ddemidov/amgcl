15df1d7be117fcd86ade3e9e7328417674401af7
Denis Demidov <dennis.demidov@gmail.com>
2019-08-01 11:22:35 +0300

    Simplify mixed precision usage
    Allow make_solver where preconditioner and solver have mixed precision.
    Only builtin and vexcl backends support this for now.

--- examples/mixed_precision.cpp
+++ examples/mixed_precision.cpp
@@ -2,6 +2,7 @@
 #include <tuple>
 
 #include <amgcl/adapter/crs_tuple.hpp>
+#include <amgcl/make_solver.hpp>
 #include <amgcl/amg.hpp>
 #include <amgcl/coarsening/smoothed_aggregation.hpp>
 #include <amgcl/relaxation/spai0.hpp>
@@ -29,65 +30,54 @@ using amgcl::prof;
 int main() {
     // Combine single-precision preconditioner with a
     // double-precision Krylov solver.
-    typedef
+    typedef amgcl::make_solver<
         amgcl::amg<
             fBackend,
             amgcl::coarsening::smoothed_aggregation,
             amgcl::relaxation::spai0
-            >
-        Precond;
-
-    // Solver is in double precision:
-    typedef
+            >,
         amgcl::solver::cg< dBackend >
+        >
         Solver;
 
     std::vector<ptrdiff_t> ptr, col;
-    std::vector<double>    val_d;
-    std::vector<double>    rhs;
+    std::vector<double>    val, rhs;
 
-    fBackend::params bprm_f;
-    dBackend::params bprm_d;
+    dBackend::params bprm;
 
 #ifdef SOLVER_BACKEND_VEXCL
     vex::Context ctx(vex::Filter::Env);
     std::cout << ctx << std::endl;
 
-    bprm_f.q = ctx;
-    bprm_d.q = ctx;
+    bprm.q = ctx;
 #endif
 
     prof.tic("assemble");
-    int n = sample_problem(128, val_d, col, ptr, rhs);
+    int n = sample_problem(128, val, col, ptr, rhs);
     prof.toc("assemble");
 
-    std::vector<float>  val_f(val_d.begin(), val_d.end());
-
-    auto A_f = std::tie(n, ptr, col, val_f);
-
 #if defined(SOLVER_BACKEND_VEXCL)
-    dBackend::matrix A_d(ctx, n, n, ptr, col, val_d);
+    dBackend::matrix A_d(ctx, n, n, ptr, col, val);
 
     vex::vector<double> f(ctx, rhs);
     vex::vector<double> x(ctx, n);
     x = 0;
 #elif defined(SOLVER_BACKEND_BUILTIN)
-    auto A_d = std::tie(n, ptr, col, val_d);
+    auto A_d = std::tie(n, ptr, col, val);
     std::vector<double> &f = rhs;
     std::vector<double> x(n, 0.0);
 #endif
 
     prof.tic("setup");
-    Solver S(n, Solver::params(), bprm_d);
-    Precond P(A_f, Precond::params(), bprm_f);
+    Solver S(std::tie(n, ptr, col, val), Solver::params(), bprm);
     prof.toc("setup");
 
-    std::cout << P << std::endl;
+    std::cout << S << std::endl;
 
     int iters;
     double error;
     prof.tic("solve");
-    std::tie(iters, error) = S(A_d, P, f, x);
+    std::tie(iters, error) = S(A_d, f, x);
     prof.toc("solve");
 
     std::cout << "Iterations: " << iters << std::endl
