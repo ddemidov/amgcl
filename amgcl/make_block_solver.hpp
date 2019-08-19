7c46cacd0967ed6c9f8912ad3e875c2fdac2d845
Denis Demidov <dennis.demidov@gmail.com>
2019-08-19 16:41:20 +0300

    Move mixed-backend matrix ops from make_block_solver.hpp to matrix_ops.hpp

--- amgcl/make_block_solver.hpp
+++ amgcl/make_block_solver.hpp
@@ -11,39 +11,6 @@ namespace amgcl {
 
 namespace backend {
 
-/* Allows to do matrix-vector products with mixed scalar/nonscalar types.
- * Reinterprets pointers to the vectors data into appropriate types.
- */
-template <class Alpha, class Matrix, class Vector1, class Beta, class Vector2>
-struct spmv_impl<
-    Alpha, Matrix, Vector1, Beta, Vector2,
-    typename std::enable_if<
-            detail::use_builtin_matrix_ops<Matrix>::value && (
-            math::static_rows<typename value_type<Matrix>::type>::value != math::static_rows<typename value_type<Vector1>::type>::value ||
-            math::static_rows<typename value_type<Matrix>::type>::value != math::static_rows<typename value_type<Vector2>::type>::value)
-        >::type
-    >
-{
-    static void apply(
-            Alpha alpha, const Matrix &A, const Vector1 &x, Beta beta, Vector2 &y
-            )
-    {
-        typedef typename value_type<Matrix>::type     val_type;
-        typedef typename math::rhs_of<val_type>::type rhs_type;
-
-        const size_t n = backend::rows(A);
-        const size_t m = backend::cols(A);
-
-        rhs_type const * xptr = reinterpret_cast<rhs_type const *>(&x[0]);
-        rhs_type       * yptr = reinterpret_cast<rhs_type       *>(&y[0]);
-
-        iterator_range<rhs_type const *> xrng(xptr, xptr + m);
-        iterator_range<rhs_type       *> yrng(yptr, yptr + n);
-
-        spmv(alpha, A, xrng, beta, yrng);
-    }
-};
-
 } // namespace backend
 
 /* Creates solver that operates in non-scalar domain but may take scalar inputs
