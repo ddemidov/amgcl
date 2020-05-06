ddemidov/amgcl

commit 5f16de96778345ffb83729651eb3c444bb99ee79

Get rid of unneeded temporary in SchurPC

amgcl/preconditioner/schur_pressure_correction.hpp
@@ -389,6 +389,9 @@ class schur_pressure_correction {
                 Kuu_dia = diagonal(*Kuu, /*invert = */true);
             }
 
+            this->Kup = backend_type::copy_matrix(Kup, bprm);
+            this->Kpu = backend_type::copy_matrix(Kpu, bprm);
+
             if (prm.adjust_p == 1) {
                 // Use (Kpp - dia(Kpu * dia(Kuu)^-1 * Kup))
                 // to setup the P preconditioner.
@@ -422,24 +425,21 @@ class schur_pressure_correction {
 
                 // Use (Kpp - Kpu * dia(Kuu)^-1 * Kup)
                 // to setup the P preconditioner.
-                build_matrix T(*Kpu);
 #pragma omp parallel for
-                for(ptrdiff_t i = 0; i < static_cast<ptrdiff_t>(np); ++i) {
-                    for(ptrdiff_t j = T.ptr[i], e = T.ptr[i+1]; j < e; ++j) {
-                        T.val[j] *= (*Kuu_dia)[T.col[j]];
+                for(ptrdiff_t i = 0; i < static_cast<ptrdiff_t>(nu); ++i) {
+                    value_type d = (*Kuu_dia)[i];
+                    for(ptrdiff_t j = Kup->ptr[i], e = Kup->ptr[i+1]; j < e; ++j) {
+                        Kup->val[j] *= d;
                     }
                 }
 
                 Kpp = backend::sum(
                         math::identity<value_type>(), *Kpp,
-                       -math::identity<value_type>(), *backend::product(T, *Kup));
+                       -math::identity<value_type>(), *backend::product(*Kpu, *Kup));
             }
 
             U = std::make_shared<USolver>(*Kuu, prm.usolver, bprm);
             P = std::make_shared<PSolver>(*Kpp, prm.psolver, bprm);
-
-            this->Kup = backend_type::copy_matrix(Kup, bprm);
-            this->Kpu = backend_type::copy_matrix(Kpu, bprm);
 
             rhs_u = backend_type::create_vector(nu, bprm);
             rhs_p = backend_type::create_vector(np, bprm);

commit f75c743b0d2edcbd4162bf2cfe2df25e6af5e792

Fix a bug introduced in 5f16de967783
Move to builtin backend is a noop, so can not really get rid of
the temporary.

amgcl/preconditioner/schur_pressure_correction.hpp
@@ -419,9 +419,6 @@ class schur_pressure_correction {
                 Kuu_dia = diagonal(*Kuu, /*invert = */true);
             }
 
-            this->Kup = backend_type::copy_matrix(Kup, bprm);
-            this->Kpu = backend_type::copy_matrix(Kpu, bprm);
-
             if (prm.adjust_p == 1) {
                 // Use (Kpp - dia(Kpu * dia(Kuu)^-1 * Kup))
                 // to setup the P preconditioner.
@@ -455,21 +452,36 @@ class schur_pressure_correction {
 
                 // Use (Kpp - Kpu * dia(Kuu)^-1 * Kup)
                 // to setup the P preconditioner.
+                backend::numa_vector<value_type> val(Kup->nnz);
+
 #pragma omp parallel for
                 for(ptrdiff_t i = 0; i < static_cast<ptrdiff_t>(nu); ++i) {
                     value_type d = (*Kuu_dia)[i];
                     for(ptrdiff_t j = Kup->ptr[i], e = Kup->ptr[i+1]; j < e; ++j) {
-                        Kup->val[j] *= d;
+                        val[j] = d * Kup->val[j];
                     }
                 }
 
+                build_matrix Kup_hat;
+
+                Kup_hat.own_data = false;
+                Kup_hat.nrows    = nu;
+                Kup_hat.ncols    = np;
+                Kup_hat.nnz      = Kup->nnz;
+                Kup_hat.ptr      = Kup->ptr;
+                Kup_hat.col      = Kup->col;
+                Kup_hat.val      = val.data();
+
                 Kpp = backend::sum(
                         math::identity<value_type>(), *Kpp,
-                       -math::identity<value_type>(), *backend::product(*Kpu, *Kup));
+                       -math::identity<value_type>(), *backend::product(*Kpu, Kup_hat));
             }
 
             U = std::make_shared<USolver>(*Kuu, prm.usolver, bprm);
             P = std::make_shared<PSolver>(*Kpp, prm.psolver, bprm);
+
+            this->Kup = backend_type::copy_matrix(Kup, bprm);
+            this->Kpu = backend_type::copy_matrix(Kpu, bprm);
 
             rhs_u = backend_type::create_vector(nu, bprm);
             rhs_p = backend_type::create_vector(np, bprm);