fb68f234223c1f1ab4963c364a1ade7c083f0c05
Denis Demidov <dennis.demidov@gmail.com>
2021-03-06 15:25:17 +0300

    Separate detection of scotch and ptscotch
    Closes #188

--- cmake/FindScotch.cmake
+++ cmake/FindScotch.cmake
@@ -7,19 +7,39 @@ if (NOT (Scotch_INCLUDES AND Scotch_LIBRARIES))
         PATH_SUFFIXES scotch
         )
 
- 
     find_library(SCOTCH_LIBRARY       scotch      PATHS $ENV{SCOTCHDIR})
-    find_library(PTSCOTCH_LIBRARY     ptscotch    PATHS $ENV{SCOTCHDIR})
     find_library(SCOTCHERR_LIBRARY    scotcherr   PATHS $ENV{SCOTCHDIR})
-    find_library(PTSCOTCHERR_LIBRARY  ptscotcherr PATHS $ENV{SCOTCHDIR})
+endif()
 
+if (NOT (PTScotch_INCLUDES AND PTScotch_LIBRARIES))
+    find_path(PTScotch_INCLUDES
+        NAMES ptscotch.h
+        PATHS $ENV{PTSCOTCHDIR}
+        PATH_SUFFIXES scotch ptscotch
+        )
+
+    find_library(PTSCOTCH_LIBRARY     ptscotch    PATHS $ENV{PTSCOTCHDIR})
+    find_library(PTSCOTCHERR_LIBRARY  ptscotcherr PATHS $ENV{PTSCOTCHDIR})
+endif()
+
+if (Scotch_INCLUDES AND SCOTCH_LIBRARY AND SCOTCHERR_LIBRARY AND
+        PTScotch_INCLUDES AND PTSCOTCH_LIBRARY AND PTSCOTCHERR_LIBRARY)
     set(Scotch_LIBRARIES
-        "${SCOTCH_LIBRARY};${PTSCOTCH_LIBRARY};${SCOTCHERR_LIBRARY};${PTSCOTCHERR_LIBRARY}"
+        "${SCOTCH_LIBRARY};${SCOTCHERR_LIBRARY};${PTSCOTCH_LIBRARY};${PTSCOTCHERR_LIBRARY}"
         )
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-    Scotch DEFAULT_MSG Scotch_INCLUDES Scotch_LIBRARIES)
 
-mark_as_advanced(Scotch_INCLUDES Scotch_LIBRARIES)
+find_package_handle_standard_args(
+    Scotch DEFAULT_MSG
+    Scotch_INCLUDES PTScotch_INCLUDES
+    Scotch_LIBRARIES PTScotch_LIBRARIES
+    )
+
+mark_as_advanced(
+    Scotch_INCLUDES
+    Scotch_LIBRARIES
+    PTScotch_INCLUDES
+    PTScotch_LIBRARIES
+    )
